5bcb46dbcb05c20b23778637cd0d2767d3f9cfd5
Lebedev Alexandr <s.lebedev.2011@bk.ru>
2019-05-10 21:09:13 +0000

    Update VideoStream_test.cpp

--- project/src/VideoStream_test.cpp
+++ project/src/VideoStream_test.cpp
@@ -1,72 +1,41 @@
 #include <iostream>
-#include <VideoStream.h>
-#include <GLFW/glfw3.h>
-#include <GL/glew.h>
-#include <ShaderProgram.h>
 #include <opencv2/opencv.hpp>
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
-VideoStream::VideoStream() {
-    //context_init();
+#include "ShaderProgram.h"
+#include "VideoStream.h"
 
+
+
+VideoStream::VideoStream(int cam_index): cam(cv::VideoCapture(cam_index)) {
     glGenTextures(1, &texture);
     glGenBuffers(1, &VBO);
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &EBO);
+    configure_VAO();
 
 }
 
 
-
-/*bool VideoStream::context_init() {
-    glfwInit();
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-
-
-    window = glfwCreateWindow(window_width, window_height, "Hello", nullptr, nullptr);
-    if (window == nullptr) {
-        std::cout << "Failed to create window" << std::endl;
-        glfwTerminate();
-        return false;
-    }
-
-    glfwMakeContextCurrent(window);
-
-    glewExperimental = GL_TRUE;
-    if (glewInit() != GLEW_OK) {
-        std::cout << "Failed to init GLEW" << std::endl;
-        return false;
-    }
-
-
-    glViewport(0, 0, window_width, window_height);
-    //glfwSetKeyCallback(window, key_callback);
-
-    return true;
-} */
-
 bool VideoStream::configure_VAO() {
 
     GLfloat vertices[] = {
-         1.0f,  1.0f, 0.0f, 1.0f, 1.0f, 
-         1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
-        -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
-        -1.0f,  1.0f, 0.0f, 0.0f, 1.0f
+            1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
+            1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+            -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+            -1.0f,  1.0f, 0.0f, 0.0f, 1.0f
     };
 
     GLuint indices[] = {
-        0, 1, 3,
-        1, 2, 3
+            0, 1, 3,
+            1, 2, 3
     };
 
-    ShaderProgram shader("project/src/v_shader.txt", "project/src/f_shader.txt");
+    shader = ShaderProgram("project/shaders/v_shader.txt", "project/shaders/f_shader.txt");
 
     glBindVertexArray(VAO);
-
 
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -80,14 +49,11 @@ bool VideoStream::configure_VAO() {
 
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*) (3 * sizeof(GLfloat)));
     glEnableVertexAttribArray(1);
-    
 
-
-    shader.use();
 
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, texture);
-    glUniform1i(glGetUniformLocation(shader.program, "ourTexture1"), 0);
+    glUniform1i(glGetUniformLocation(shader.get_program(), "ourTexture1"), 0);
 
 
     glBindVertexArray(0);
@@ -95,42 +61,23 @@ bool VideoStream::configure_VAO() {
 
 }
 
-
-/*void VideoStream::key_callback(int key, int scancode, int action, int mode) {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, GL_TRUE);
-    }
-} */
+//TODO(me): add destructor
+VideoStream::~VideoStream() {}
 
 
-/* void VideoStream::lock_frame_rate(double frame_rate) {
-    double allowed_frame_time = 1.0 / frame_rate;  // Продолжительность одного кадра
-
-    frame_end_time = glfwGetTime();
-    double frame_draw_time = frame_end_time - frame_start_time;
-
-    double sleep_time = 0.0;
-
-    if (frame_draw_time < allowed_frame_time) {
-        sleep_time = allowed_frame_time - frame_draw_time;
-        usleep(1000000 * sleep_time);
-    }
-} */
-
-void VideoStream::draw_video_frame() {
-
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-
+void VideoStream::render() {
+    cam.read(frame);
     mat_to_texture();
+    shader.use();
 
     glBindTexture(GL_TEXTURE_2D, texture);
-    
+
     glBindVertexArray(VAO);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    
+
     glBindVertexArray(0);
+    
+    glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 
@@ -139,7 +86,7 @@ void VideoStream::mat_to_texture() {
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  
+
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
