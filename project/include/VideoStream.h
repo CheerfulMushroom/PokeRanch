fbe8afb635d54f514c813de684e09135e2a00462
Lebedev Alexandr <s.lebedev.2011@bk.ru>
2019-05-10 21:12:22 +0000

    Update VideoStream.h

--- project/include/VideoStream.h
+++ project/include/VideoStream.h
@@ -3,45 +3,38 @@
 
 #include <iostream>
 #include <unistd.h>
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <ShaderProgram.h>
 #include <opencv2/opencv.hpp>
 
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 
-class VideoStream {
+#include "ShaderProgram.h"
+#include "VideoStream.h"
+#include "Interfaces.h"
+
+
+class VideoStream: public Renderable {
  public:
-    VideoStream();
+    VideoStream(int cam_index);
+    ~VideoStream();
 
-    void draw_video_frame();
-
-    //bool context_init();
-
+    void render();
+    
     bool configure_VAO();
-
-    //void lock_frame_rate(double frame_rate);
-
-    //GLFWwindow *window;
-
-    //double frame_start_time;
-    //double frame_end_time;
-
-    cv::Mat frame;
-
-    GLuint texture;
-    GLuint VBO;
-    GLuint VAO;
-    GLuint EBO;
 
 
  private:
     void mat_to_texture();
 
-    //int window_width;
-    //int window_height;
-    
+    ShaderProgram shader;
+    cv::VideoCapture cam;
+    cv::Mat frame;
+    GLuint texture;
+    GLuint VBO;
+    GLuint VAO;
+    GLuint EBO;
 };
 
-#endif
+#endif // PROJECT_INCLUDE_VIDEOSTREAM_H_
