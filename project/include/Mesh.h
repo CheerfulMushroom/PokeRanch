7710d1d18356ed181a82014b238e2c545e5bbf90
Anton Chetverov <678summerdays@gmail.com>
2019-05-19 22:16:20 +0300

    nothing

--- project/include/Mesh.h
+++ project/include/Mesh.h
@@ -45,7 +45,7 @@ class Mesh {
     std::vector<Vertex> vertices;
     std::vector<unsigned int> indices;
     std::vector<Texture> textures;
-    std::vector<VertexBoneData> bones;
+    //std::vector<VertexBoneData> bones;
 
     Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
     //Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures, std::vector<VertexBoneData> bones);
